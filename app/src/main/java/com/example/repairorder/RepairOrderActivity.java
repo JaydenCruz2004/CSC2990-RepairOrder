d2c84d7bc8aecd0ace3e6e10e88927aeacf85be8
Jayden <jaydencruz824@gmail.com>
2025-09-02 12:17:41 -0500

    un-hardcoded the text strings values

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.repairorder;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class RepairOrderActivity extends AppCompatActivity {
 
+    double numbers =0.0;
 //onCreate is the starting point of the activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        TextView totalTV;
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
@@ -20,6 +30,18 @@ public class RepairOrderActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+        totalTV = findViewById(R.id.textViewSub2);
+
+        Random gen = new Random();
+        double number = gen.nextDouble();
+        String v1 = "$" + number;
+        totalTV.setText(v1);
+
+        String value = totalTV.getText().toString();
+
+        Log.i("TEST", value);
     }
 }
